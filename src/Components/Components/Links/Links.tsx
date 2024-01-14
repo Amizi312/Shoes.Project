c6ded92008738485688242528ab00e45e37be2ab
Amichai <aamizii@gmail.com>
2024-01-14 16:22:51 +0200

    connection to github

---
+++ src/Components/Components/Links/Links.tsx
@@ -0,0 +1,11 @@
+import "./Links.css";
+
+function Links(): JSX.Element {
+    return (
+        <div className="Links">
+			
+        </div>
+    );
+}
+
+export default Links;
---
+++ src/Components/Components/Youtube/Youtube.tsx
@@ -0,0 +1,11 @@
+import "./Youtube.css";
+
+function Youtube(): JSX.Element {
+    return (
+        <div className="Youtube">
+			
+        </div>
+    );
+}
+
+export default Youtube;
